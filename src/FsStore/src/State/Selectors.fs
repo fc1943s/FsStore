63429adfd53860a08e02a33edb4c9b62f85576ef
fc1943s <root@fc1943s.dev>
2021-09-04 04:01:13 -0300

    dep

--- src/FsStore/src/State/Selectors.fs
+++ src/FsStore/src/State/Selectors.fs
@@ -15,26 +15,6 @@ open FsStore.Bindings
 #nowarn "40"
 
 
-module rec Sync =
-    [<RequireQualifiedAccess>]
-    type Request =
-        | Connect of alias: string
-        | Set of alias: string * atomPath: string * value: string
-        | Get of alias: string * atomPath: string
-        | Filter of alias: string * storeRoot: string * collection: string
-
-    [<RequireQualifiedAccess>]
-    type Response =
-        | ConnectResult
-        | SetResult of ok: bool
-        | GetResult of value: string option
-        | GetStream of atomPath: string * value: string option
-        | FilterResult of atomPathArray: string []
-        | FilterStream of (string * string * string) * atomPathArray: string []
-
-    let endpoint = $"/{nameof Sync}"
-
-
 [<AutoOpen>]
 module SelectorsMagic =
     module Selectors =
