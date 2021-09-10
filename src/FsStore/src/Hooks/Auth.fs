11aeff702147639338acc3f891853baddb63b5b2
fc1943s <root@fc1943s.dev>
2021-09-10 17:42:24 -0300

    async auth

--- src/FsStore/src/Hooks/Auth.fs
+++ src/FsStore/src/Hooks/Auth.fs
@@ -21,6 +21,19 @@ module Auth =
                     setter
                     (Message.Command (AppCommand.QueueNotification (Notification.Error ("User Error", error, None))))
 
+    module Actions =
+        let logout =
+            Atom.Primitives.setSelector
+                (fun getter setter () ->
+                    Profiling.addTimestamp
+                        (fun () -> $"{nameof FsStore} | Auth.Actions.logout. invoking gun.user().leave()")
+                        getLocals
+
+                    let gunUser = Atom.get getter Selectors.Gun.gunUser
+                    gunUser.leave ()
+                    Atom.change setter Atoms.gunTrigger ((+) 1)
+                    Atom.change setter Atoms.hubTrigger ((+) 1))
+
     let signIn =
         fun getter _setter (alias, password) ->
             promise {
@@ -60,145 +73,127 @@ module Auth =
                     | { err = Some error } -> Error error
             }
 
-    module Actions =
-        let logout =
-            Atom.Primitives.setSelector
-                (fun getter setter () ->
-                    Profiling.addTimestamp
-                        (fun () -> $"{nameof FsStore} | Auth.Actions.logout. invoking gun.user().leave()")
-                        getLocals
 
-                    let gunUser = Atom.get getter Selectors.Gun.gunUser
-                    gunUser.leave ()
-                    Atom.change setter Atoms.gunTrigger ((+) 1)
-                    Atom.change setter Atoms.hubTrigger ((+) 1))
+    let changePassword getter setter (password, newPassword) =
+        promise {
+            let alias = Atom.get getter Selectors.Gun.alias
+            let gunUser = Atom.get getter Selectors.Gun.gunUser
 
+            let! result =
+                promise {
+                    match alias with
+                    | Some alias ->
+                        let! ack = Gun.changeUserPassword gunUser alias (Gun.Pass password) (Gun.Pass newPassword)
 
-        let changePassword =
-            Atom.Primitives.asyncSetSelector
-                (fun getter setter (password, newPassword) ->
-                    promise {
-                        let alias = Atom.get getter Selectors.Gun.alias
+                        return!
+                            promise {
+                                match ack with
+                                | { ok = Some 1; err = None } ->
+                                    Atom.change setter Atoms.gunTrigger ((+) 1)
+                                    Atom.change setter Atoms.hubTrigger ((+) 1)
+                                    return Ok alias
+                                | { err = Some error } -> return Error error
+                                | _ -> return Error $"invalid ack {JS.JSON.stringify ack}"
+                            }
+                    | _ -> return Error "Invalid alias"
+                }
+
+            result
+            |> Utils.hydrateAppMessage setter (fun alias -> Message.Event (AppEvent.UserPasswordChanged alias))
+            |> ignore
+
+            return result
+        }
+
+    let inline deleteUser getter setter password =
+        promise {
+            let alias = Atom.get getter Selectors.Gun.alias
+
+            let! result =
+                promise {
+                    match alias with
+                    | Some alias ->
                         let gunUser = Atom.get getter Selectors.Gun.gunUser
 
-                        let! result =
+                        let! ack = Gun.deleteUser gunUser alias (Gun.Pass password)
+                        printfn $"ack={JS.JSON.stringify ack}"
+
+                        return!
                             promise {
-                                match alias with
-                                | Some alias ->
-                                    let! ack =
-                                        Gun.changeUserPassword gunUser alias (Gun.Pass password) (Gun.Pass newPassword)
-
-                                    return!
-                                        promise {
-                                            match ack with
-                                            | { ok = Some 1; err = None } ->
-                                                Atom.change setter Atoms.gunTrigger ((+) 1)
-                                                Atom.change setter Atoms.hubTrigger ((+) 1)
-                                                return Ok alias
-                                            | { err = Some error } -> return Error error
-                                            | _ -> return Error $"invalid ack {JS.JSON.stringify ack}"
-                                        }
-                                | _ -> return Error "Invalid alias"
+                                match ack with
+                                | { ok = Some 0; err = None } ->
+                                    Atom.set setter Actions.logout ()
+                                    return Ok alias
+                                | { err = Some error } -> return Error error
+                                | _ -> return Error $"invalid ack {JS.JSON.stringify ack}"
                             }
+                    | _ -> return Error "Invalid alias"
 
-                        result
-                        |> Utils.hydrateAppMessage
-                            setter
-                            (fun alias -> Message.Event (AppEvent.UserPasswordChanged alias))
-                        |> ignore
-                    })
+                }
 
-        let deleteUser =
-            Atom.Primitives.asyncSetSelector
-                (fun getter setter password ->
-                    promise {
-                        let alias = Atom.get getter Selectors.Gun.alias
+            result
+            |> Utils.hydrateAppMessage setter (fun alias -> Message.Event (AppEvent.UserDeleted alias))
+            |> ignore
 
-                        let! result =
-                            promise {
-                                match alias with
-                                | Some alias ->
-                                    let gunUser = Atom.get getter Selectors.Gun.gunUser
+            return result
 
-                                    let! ack = Gun.deleteUser gunUser alias (Gun.Pass password)
-                                    printfn $"ack={JS.JSON.stringify ack}"
+        }
 
-                                    return!
-                                        promise {
-                                            match ack with
-                                            | { ok = Some 0; err = None } ->
-                                                Atom.set setter logout ()
-                                                return Ok alias
-                                            | { err = Some error } -> return Error error
-                                            | _ -> return Error $"invalid ack {JS.JSON.stringify ack}"
-                                        }
-                                | _ -> return Error "Invalid alias"
+    let inline signUp getter setter (alias, password) =
+        promise {
+            let! result =
+                promise {
+                    if alias = "" || password = "" then
+                        return Error "Required fields"
+                    elif (JSe.RegExp @"^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$")
+                             .Test alias
+                         |> not then
+                        return Error "Invalid email address"
+                    else
+                        let gun = Atom.get getter Selectors.Gun.gun
+                        let logger = Atom.get getter Selectors.logger
+                        let user = gun.user ()
 
-                            }
+                        let getLocals () =
+                            $"gunUser.is={user.is |> Js.objectKeys} {getLocals ()}"
 
-                        result
-                        |> Utils.hydrateAppMessage setter (fun alias -> Message.Event (AppEvent.UserDeleted alias))
-                        |> ignore
-
-                    })
-
-        let signUp =
-            Atom.Primitives.asyncSetSelector
-                (fun getter setter (alias, password) ->
-                    promise {
-                        let! result =
-                            promise {
-                                if alias = "" || password = "" then
-                                    return Error "Required fields"
-                                elif (JSe.RegExp @"^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$")
-                                         .Test alias
-                                     |> not then
-                                    return Error "Invalid email address"
-                                else
-                                    let gun = Atom.get getter Selectors.Gun.gun
-                                    let logger = Atom.get getter Selectors.logger
-                                    let user = gun.user ()
-
-                                    let getLocals () =
-                                        $"gunUser.is={user.is |> Js.objectKeys} {getLocals ()}"
-
-                                    logger.Debug (fun () -> "Auth.useSignUp") getLocals
+                        logger.Debug (fun () -> "Auth.useSignUp") getLocals
 
 
-                                    match! signIn getter setter (alias, password) with
-                                    | Ok (alias, keys) -> return Ok (alias, keys)
-                                    | Error error ->
-                                        let! ack = Gun.createUser user (Gun.Alias alias) (Gun.Pass password)
+                        match! signIn getter setter (alias, password) with
+                        | Ok (alias, keys) -> return Ok (alias, keys)
+                        | Error error ->
+                            let! ack = Gun.createUser user (Gun.Alias alias) (Gun.Pass password)
 
-                                        let getLocals () =
-                                            $"error={error} signUpAck={JS.JSON.stringify ack} {getLocals ()}"
+                            let getLocals () =
+                                $"error={error} signUpAck={JS.JSON.stringify ack} {getLocals ()}"
 
-                                        logger.Debug (fun () -> "Auth.useSignUp. Gun.createUser") getLocals
+                            logger.Debug (fun () -> "Auth.useSignUp. Gun.createUser") getLocals
 
-                                        return!
-                                            promise {
-                                                match ack with
-                                                | {
-                                                      err = None
-                                                      ok = Some 0
-                                                      pub = Some _
-                                                  } ->
-                                                    match! signIn getter setter (alias, password) with
-                                                    | Ok (alias, keys) ->
-                                                        do! Gun.putPublicHash gun alias
-                                                        return Ok (alias, keys)
-                                                    | Error error -> return Error error
-                                                | { err = Some err } -> return Error err
-                                                | _ -> return Error $"Invalid ack: {JS.JSON.stringify ack}"
-                                            }
-                            }
+                            return!
+                                promise {
+                                    match ack with
+                                    | {
+                                          err = None
+                                          ok = Some 0
+                                          pub = Some _
+                                      } ->
+                                        match! signIn getter setter (alias, password) with
+                                        | Ok (alias, keys) ->
+                                            do! Gun.putPublicHash gun alias
+                                            return Ok (alias, keys)
+                                        | Error error -> return Error error
+                                    | { err = Some err } -> return Error err
+                                    | _ -> return Error $"Invalid ack: {JS.JSON.stringify ack}"
+                                }
+                }
 
-                        result
-                        |> Utils.hydrateAppMessage
-                            setter
-                            (fun (alias, _keys) -> Message.Event (AppEvent.UserRegistered alias))
-                        |> ignore
-                    })
+            result
+            |> Utils.hydrateAppMessage setter (fun (alias, _keys) -> Message.Event (AppEvent.UserRegistered alias))
+            |> ignore
+
+            return result
+        }
 
     let inline useGunAliasLoader () =
         let setInternalAlias = Store.useSetState Atoms.internalAlias
